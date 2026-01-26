46a80f8af25e1c07cd529815b637bff32b6908ec
skarsden <zachfrancis98@gmail.com>
2026-01-26 13:23:16 -0500

    Simple TextNode unit tests

--- src/textnode.py
+++ src/textnode.py
@@ -20,4 +20,4 @@ class TextNode():
                 self.url == other.url)
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
