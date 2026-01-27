fae1e6c21e390dc732c3de7c87b5a16d6de9716d
skarsden <zachfrancis98@gmail.com>
2026-01-27 12:26:32 -0500

    added link and image parsing functions nad tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,4 @@
-from textnode import TextNode, TextType
-from htmlnode import LeafNode
-
 def main():
-    node = LeafNode(tag="p", value="This is some anchor text")
-    node2 = LeafNode(tag="a", value="Click Here", props={"href": "https://www.google.com"})
-    print(node.to_html())
-    print(node2.to_html())
+    print("Hello World")
 
 main()
