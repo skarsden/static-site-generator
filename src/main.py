3f433f86c76200871a93e5a689743a58f2673330
skarsden <zachfrancis98@gmail.com>
2026-01-29 16:22:08 -0500

    configured to host web page on GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import shutil
 import os
+import sys
 from markdown_blocks import extract_title, markdown_to_html_node
 
 def copy_static(src, dest):
-    if dest == "./public":
-        shutil.rmtree(dest)
-        os.mkdir("./public")
+    if dest == "./docs":
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+        os.mkdir("./docs")
+        
     for item in os.listdir(src):
         file_path = os.path.join(src, item)
         if os.path.isfile(file_path):
@@ -16,7 +19,7 @@ def copy_static(src, dest):
             os.mkdir(new_dest)
             copy_static(new_src, new_dest)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md = ""
     template = ""
@@ -30,25 +33,28 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md).lstrip("# ")
     web_page = template.replace("{{ Title }}", title)
     web_page = web_page.replace("{{ Content }}", html)
+    web_page = web_page.replace("href=\"/", f"href=\"{basepath}")
+    web_page = web_page.replace("src=\"/", f"src=\"{basepath}")
 
     with open(dest_path, "x") as file:
         file.write(web_page)
     
 
-def generate_pages_recursive(dir_path_content, template_path, dir_path_dest):
+def generate_pages_recursive(dir_path_content, template_path, dir_path_dest, basepath):
     new_content = ""
     new_dest = ""
     for item in os.listdir(dir_path_content):
         new_content = os.path.join(dir_path_content, item)
         new_dest = os.path.join(dir_path_dest, item)
         if os.path.isfile(new_content):
-            generate_page(new_content, template_path, new_dest.replace(".md", ".html"))
+            generate_page(new_content, template_path, new_dest.replace(".md", ".html"), basepath)
         else:
             os.mkdir(new_dest)
-            generate_pages_recursive(new_content, template_path, new_dest)
+            generate_pages_recursive(new_content, template_path, new_dest, basepath)
 
 def main():
-    copy_static("./static", "./public")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    basepath = "/" if len(sys.argv) == 1 else sys.argv[1]
+    copy_static("./static", "./docs")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 main()
